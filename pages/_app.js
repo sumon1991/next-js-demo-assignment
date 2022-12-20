d27df6934592062dd99a88e0cfce6af1f6a641c8
Sumon Sen <Sumon.Sen@edynamic.net>
2022-12-20 11:18:24 +0530

    fixed some issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import Layout from '../common/Layout'
 import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import {Montserrat} from '@next/font/google';
+
+const roboto = Montserrat({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+});
 
 // function MyApp({ Component, pageProps }) {
 //   return <Component {...pageProps} />
@@ -9,7 +16,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 function MyApp({ Component, pageProps }) {
   return(
     <Layout footerstatus={pageProps.footerstatus}> 
+    <div className={roboto.className}>
       <Component {...pageProps} />
+    </div>
     </Layout>
   )
 }
